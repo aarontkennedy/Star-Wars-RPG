f4a30cac48bbdc63c19ac6b5e7c13a3d285b66db
Aaron Kennedy <aarontkennedy@MacBook-Air.local>
2018-03-31 19:02:48 -0500

    game is virtually done, added and improved modal dialog over using alert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,55 @@
 $(document).ready(function () {
 
+    // start the music theme
+    $('audio#starWarsTheme')[0].play();
+    $('audio#starWarsTheme')[0].volume = 0.3;
+    $('audio#starWarsTheme').hide();
+    // sound button toggles visibility of the audio control
+    $("#soundCtrlButton").click(function () {
+        $('audio#starWarsTheme').toggle();
+    });
+
+
+
+    // End Game Dialog Modal Window Pop-up thing Class
+    function MyModal() {
+        this.id = "gameEndDialog";
+        this.htmlElement = $("#" + this.id);
+        this.htmlElement.hide();
+    }
+
+    MyModal.prototype.hide = function (isGoodMessage, message) {
+        this.htmlElement.hide();
+    }
+
+    MyModal.prototype.show = function (isGoodMessage, message) {
+        // this part controls the background of the modal
+        // if you win, you get Yoda
+        if (isGoodMessage) {
+            this.htmlElement.css("background-image",
+                "url(./assets/images/yodaBackground.png)");
+        }
+        else { // if you lose, you get the Emperor
+            this.htmlElement.css("background-image",
+                "url(./assets/images/palpatineBackdrop.jpg)");
+        }
+        $("#gameMessage").text(message); // display the message
+
+        // This creates an event that listens for the clicking of
+        // the dialog window.  Clicking the window closes it and 
+        // resets the game.
+        this.htmlElement.on('click', function () {
+            resetGame();
+            gameEndDialogModal.hide();
+        });
+
+        this.htmlElement.show();
+    }
+
+    var gameEndDialogModal = new MyModal();
+
+
+
     // base class for all the chacters
     function Character(id, name, health, attackPower, weapon) {
         this.kID = id; // should never change
@@ -21,26 +71,32 @@ $(document).ready(function () {
         this.damaged(0); // prints the current health
     };
 
+    // the weapon is the button clicked to attack the enemy, show it
     Character.prototype.displayWeapon = function () {
         this.kElement.after("<img class='weapon' src='assets/images/" + this.kWeapon + ".png'>");
     }
 
+    // we must not be battling, hide the weapon
     Character.prototype.hideWeapon = function () {
         $("img.weapon").hide();
     }
 
+    // return the value of your attacking power
+    // increment the attack power
     Character.prototype.attack = function () {
         var oldAttackPower = this.currentAttackPower;
         this.currentAttackPower += this.kAttackPower; // getting stronger!
         return oldAttackPower;
     };
 
+    // return the counterattack value
     Character.prototype.counterAttack = function () {
         return this.kAttackPower;
     };
 
     Character.prototype.damaged = function (damage) {
         this.currentHealth -= damage;
+        // ensure their health doesn't go negative
         if (this.currentHealth < 0) {
             this.currentHealth = 0;
         }
@@ -72,23 +128,36 @@ $(document).ready(function () {
         return true;
     };
 
-    var characters = [
-        new Character("luke", "Luke Skywalker", 100, 6, "saber"),
-        new Character("han", "Han Solo", 100, 6, "blaster"),
-        new Character("obiwan", "Obi-Wan Kenobi", 100, 6, "saber"),
-        new Character("vader", "Darth Vader", 100, 6, "saber")
-    ];
-    var chosenCharacter = null;
-    var defendingCharacter = null;
 
+
+    // this is an array of the characters to be used by the game
+    var characters = [
+        new Character("luke", "Luke Skywalker", 100, 8, "saber"),
+        new Character("han", "Han Solo", 115, 9, "blaster"),
+        new Character("obiwan", "Obi-Wan Kenobi", 120, 10, "saber"),
+        new Character("vader", "Darth Vader", 125, 12, "saber")
+    ];
+    var chosenCharacter = null;  // quick access to the character chosen
+    var defendingCharacter = null; // quick access to the current enemy
+
+    // checks if all the enemies are dead and the game is done
     function areAllEnemiesDead() {
         var allAreDead = true;
         for (var i = 0; i < characters.length; i++) {
+            // don't check our character
             if (characters[i].kID != chosenCharacter.kID) {
                 allAreDead = characters[i].isDead() && allAreDead;
             }
         }
         return allAreDead;
+    }
+
+    // do some sort of reset, reset characters and go back to beginning of game
+    function resetGame() {
+        for (var i = 0; i < characters.length; i++) {
+            characters[i].reset();
+        }
+        goToState1();
     }
 
 
@@ -112,12 +181,16 @@ $(document).ready(function () {
     var battleDisplayText = new BattleDisplay();
 
 
+
+    // this is the actual code to run the game
+    // there are 4 states that control the steps of the game
     console.log("State 1: Wait for a character to be chosen");
     function goToState1() {
         battleDisplayText.print("Choose a player to start.");
+        // set up an event to listen for a character to be chosen
         $('.card').on('click', function () {
-            $('.card').off();
-            console.log(this.id + " was chosen.");
+            $('.card').off(); // turn off the event so they can't choose more than 1
+            //console.log(this.id + " was chosen.");
             for (var i = 0; i < characters.length; i++) {
                 if (this.id == characters[i].kID) {
                     chosenCharacter = characters[i];
@@ -130,10 +203,12 @@ $(document).ready(function () {
             goToState2();
         });
     }
-    goToState1();
+    goToState1(); // we first start at State 1
 
     console.log("State 2: Wait for a character to chosen to fight");
     function goToState2() {
+        // set up an even to listen to the characters and specifically 
+        // for one of the enemies to be chosen
         $('.card').on('click', function () {
             console.log(this.id + " was chosen to fight.");
             for (var i = 0; i < characters.length; i++) {
@@ -143,41 +218,46 @@ $(document).ready(function () {
                     // character again
                 }
                 else if (this.id == characters[i].kID) {
-                    $('.card').off();
+                    $('.card').off();  // they chose an enemy, stop listening
                     defendingCharacter = characters[i];
+                    // prepare for battle
                     defendingCharacter.moveToDefender();
                     chosenCharacter.displayWeapon();
                     goToState3();
                 }
                 else {
+                    // these characters weren't clicked
                     // leave the other characters there
-                    $('.card').off();
+                    $('.card').off(); // don't listen for more
                 }
             }
-
         });
     }
 
     console.log("State 3: Time to battle");
     function goToState3() {
         battleDisplayText.print("Attack!");
+
+        // listen for your character's weapon being clicked
         $('img.weapon').on('click', function () {
+            // actual battle
             var youInflicted = defendingCharacter.damaged(chosenCharacter.attack());
             var youTookDamage = chosenCharacter.damaged(defendingCharacter.counterAttack());
-
+            // create string of battle results
             var battleResultsString = "<p>You attacked " + defendingCharacter.kName + " for " + youInflicted + " points damage.</p>";
 
             battleResultsString += "<p>" + defendingCharacter.kName + " counterattacked for " + youTookDamage + " points damage.</p>";
 
             battleDisplayText.print(battleResultsString);
 
+            // did you die?
             if (chosenCharacter.isDead()) {
                 goToState4(false);
             }
-            else if (areAllEnemiesDead()) {
+            else if (areAllEnemiesDead()) { // did you defeat all enemies?
                 goToState4(true);
             }
-            else if (defendingCharacter.isDead()) {
+            else if (defendingCharacter.isDead()) { // did you defeat this enemy?
                 battleDisplayText.print("<p>You defeated " + defendingCharacter.kName + "!</p><p>Choose an enemy to battle.</p>");
                 defendingCharacter.dies();
                 chosenCharacter.hideWeapon();
@@ -189,16 +269,11 @@ $(document).ready(function () {
     console.log("State 4: End Game");
     function goToState4(playerWon) {
         if (playerWon) {
-            alert("You Win!");
+            gameEndDialogModal.show(true, "Congratulations! You have brought balance to the force.  May the Force be with you!");
         }
         else {
-            alert("Game Over!");
+            gameEndDialogModal.show(false, "Ha, ha, ha!  You have been defeated.  Now you know the real power of the dark side.");
         }
-        // do some sort of reset
-        for (var i = 0; i < characters.length; i++) {
-            characters[i].reset();
-        }
-        goToState1();
     }
 
 });
